9d67fe07519e3ad4eeb3250c5f229f5f53edfe35
cobbn <168705970+cobbn@users.noreply.github.com>
2024-12-20 02:32:46 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -294,7 +294,7 @@ def get_readable_message():
     button = buttons.build_menu(3)
     msg += BotTheme('FREE', free=get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free), free_p=round(100-disk_usage(config_dict['DOWNLOAD_DIR']).percent, 1))
     msg += BotTheme('uptime', uptime=get_readable_time(time() - botStartTime))
-    msg += BotTheme('traf')
+    msg += BotTheme('traf' traf=get_readable_file_size(net_io_counters().bytes_sent + net_io_counters().bytes_recv))
     return msg, button
 
 
