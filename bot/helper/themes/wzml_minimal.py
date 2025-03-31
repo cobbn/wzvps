ce1abbe6eb5fcf3770d42c314d88e98d4b105f25
cobbn <168705970+cobbn@users.noreply.github.com>
2025-04-01 01:22:28 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -6,17 +6,17 @@ class WZMLStyle:
     ST_BN1_URL = 'https://t.me/acaatr'
     ST_BN2_NAME = 'Updates'
     ST_BN2_URL = 'https://t.me/Reaperzclub'
-    ST_MSG = '''<b>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</b>
-<b>Type {help_command} to get a list of available commands</b>'''
-    ST_BOTPM = '''<b>Now, This bot will send all your files and links here. Start Using ...</b>'''
-    ST_UNAUTH = '''<b>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</b>'''
-    OWN_TOKEN_GENERATE = '''<b>Temporary Token is not yours!</b>\n\n<i>Kindly generate your own.</b>'''
-    USED_TOKEN = '''<b>Temporary Token already used!</b>\n\n<i>Kindly generate a new one.</b>'''
-    LOGGED_PASSWORD = '''<b>Bot Already Logged In via Password</b>\n\n<i>No Need to Accept Temp Tokens.</b>'''
+    ST_MSG = <b>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</b>
+<b>Type {help_command} to get a list of available commands</b
+    ST_BOTPM = <b>Now, This bot will send all your files and links here. Start Using ...</b>
+    ST_UNAUTH = <b>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</b>
+    OWN_TOKEN_GENERATE = <b>Temporary Token is not yours!</b>\n\n<i>Kindly generate your own.</b>
+    USED_TOKEN = <b>Temporary Token already used!</b>\n\n<i>Kindly generate a new one.</b>
+    LOGGED_PASSWORD = <b>Bot Already Logged In via Password</b>\n\n<i>No Need to Accept Temp Tokens.</b>
     ACTIVATE_BUTTON = 'Activate Temporary Token'
     TOKEN_MSG = <b><u>Generated Temporary Login Token!</u></b>
 <b>Temp Token:</b> <code>{token}</code>
-<b>Validity:</b> {validity}"""
+<b>Validity:</b> {validity}
     # ---------------------
     # async def token_callback(_, query): ---> __main__.py
     ACTIVATED = "✅️ Activated ✅"
